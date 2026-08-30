36439d2759f703b4ae043951ae4d8cd65dfff546
anethema <anethema@users.noreply.github.com>
2026-08-30 02:01:37 -0700

    Default generation batches to 64

--- tests/test_gradio_reference.py
+++ tests/test_gradio_reference.py
@@ -42,8 +42,8 @@ class GradioReferencePreviewTests(unittest.TestCase):
     def test_clearing_reference_clears_player(self):
         self.assertIsNone(gradio_tts_app.prepare_reference_preview(None))
 
-    def test_batch_slider_defaults_to_32_and_allows_64(self):
-        self.assertEqual(gradio_tts_app.batch_size.value, 32)
+    def test_batch_slider_defaults_to_and_allows_64(self):
+        self.assertEqual(gradio_tts_app.batch_size.value, 64)
         self.assertEqual(gradio_tts_app.batch_size.maximum, 64)
 
 
